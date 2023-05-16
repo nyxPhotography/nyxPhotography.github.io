ae1d1b655e9aad057244a872312b6197e5f9b32b
Dylan Todd <dmtodd@protonmail.com>
2023-05-16 15:55:53 -0500

    Enhancement, Optimization, and Customization of Nyx Website
    * Optimized meta tags for SEO
    * Adjusted / formatted navBar logo
    * Altered buttons
    * Added "The Experience" section
    * Added "Our Work" details
    * Removed template portfolio photos
    * Adjusted contact form
    * Added "Get in Touch" section
    * Adjusted footer
    * Adjusted CSS
    * Optimized mobile view
    * Added js function for slow scroll to top of screen, linked to logos
    * Edited navBar underline in js
    * Added logo and logo w/ name .pngs

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 var width = $(window).width(); 
 window.onscroll = function(){
-if ((width >= 1000)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#header").css("background","#fff");
-        $("#header").css("color","#000");
-        $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
-        $("#header").css("padding","4vh 4vw");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid rgb(255, 44, 90)");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
-    }else{
-        $("#header").css("background","transparent");
-        $("#header").css("color","#fff");
-        $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
-        $("#header").css("padding","6vh 4vw");
-        $("#navigation a").hover(function(){
-            $(this).css("border-bottom","2px solid #fff");
-        },function(){
-            $(this).css("border-bottom","2px solid transparent");
-        });
+    if ((width >= 1000)){
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $("#header").css("background","#fff");
+            $("#header").css("color","#251825");
+            $("#header").css("box-shadow","0px 0px 20px rgba(0,0,0,0.09)");
+            $("#header").css("padding","2vh 4vw");
+            
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid #251825");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });            
+        }else{
+            $("#header").css("background","transparent");
+            $("#header").css("color","#fff");
+            $("#header").css("box-shadow","0px 0px 0px rgba(0,0,0,0)");
+            $("#header").css("padding","6vh 4vw");
+            
+            $("#navigation a").hover(function(){
+                $(this).css("border-bottom","2px solid #fff");
+            },function(){
+                $(this).css("border-bottom","2px solid transparent");
+            });
+        }
     }
-}
 }
 
 function magnify(imglink){
@@ -64,4 +66,10 @@ $(document).ready(function(){
        } 
       });
   });
-  
+
+  $(document).ready(function(){
+    $("#logo a, .footer-item a").click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 'slow');
+    });
+});
